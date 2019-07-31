d0e580b865dc5b6aee22c2ae16245883bd929408
mbj417 <martin.bjorklund@arctoslabs.com>
2019-07-31 10:31:55 +0200

    Added server and config modules

---
+++ osm_pla/server/server.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+##
+# Copyright 2019 ArctosLabs Scandinavia AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+##
+
+import asyncio
+import yaml
+import logging
+import logging.handlers
+import getopt
+import sys
+from time import time, sleep
+
+from osm_common import dbmemory, dbmongo, fslocal, msglocal, msgkafka
+from osm_common import version as common_version
+from osm_common.msgbase import MsgException
+
+from osm_pla.config.config import Config
+
+__author__ = "Martin Bjorklund"
+log = logging.getLogger(__name__)
+
+class Server:
+
+    def __init__(self, config: Config, loop=None):
+        self.msgBus = None
+        self.config = config
+        self.loop = loop or asyncio.get_event_loop()
+
+        try:
+            if config.get('message', 'driver') == "local":
+                self.msgBus = msglocal.MsgLocal()
+            elif config.get('message', 'driver') == "kafka":
+                self.msgBus = msgkafka.MsgKafka()
+            else:
+                raise Exception("Invalid message bus driver {}".format(
+                    config.get('message', 'driver')))
+            self.msgBus.connect(config.get('message'))
+        except Exception as e:
+            log.error("kafka setup error. Exception: {}".format(e))
+
+    def handle_kafka_command(self, topic, command, params):
+        log.info("Kafka message arrived: %s", values)
+
+    async def kafka_read(self):
+        log.info("Task kafka_read start")
+        while True:
+            try:
+                topics = ("pla")
+                await self.msgBus.aioread(topics, self.loop, self.handle_kafka_command)
+            except Exception as e:
+                log.error("kafka read error. Exception: {}".format(e))
+                await asyncio.sleep(5, loop=self.loop)
+
+        log.info("Task kafka_read exit")
+
+    def run(self):
+        self.loop.run_until_complete(self.kafka_read())
+        self.loop.close()
+        self.loop = None
+        if self.msgBus:
+            self.msgBus.disconnect()
